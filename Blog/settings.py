b305949d1e923efb1067bfb975f8f7dc6ecef8d7
Prabhu K S <74537989+prabhuksgowda@users.noreply.github.com>
2021-04-20 13:57:56 +0530

    first commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -124,19 +124,18 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR/'static'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR,'static'),
-)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
